a790c63b45f645568e057981490ea6cbecb635cc
bridgetorr17 <bridgetorr1902@gmail.com>
2025-05-18 14:00:06 -0400

    testing my own model

--- recognition.js
+++ recognition.js
@@ -1,16 +1,19 @@
 const axios = require("axios");
+const dotenv = require("dotenv")
+
+require('dotenv').config();
 
 axios({
     method: "POST",
-    url: "https://serverless.roboflow.com/cats-n9b87/3",
+    url: "https://serverless.roboflow.com/cats-1dq9b/5",
     params: {
-        api_key: "3Hq1uERhmVMjIMbmB10u",
-        image: "https://l4e1pa0te997vcur.public.blob.vercel-storage.com/IMG_3994-gusyTiqmEBLBewFvDSn5imR2Y3xo3g.JPG"
+        api_key: process.env.CAT_RECOGNITON_KEY,
+        image: "https://l4e1pa0te997vcur.public.blob.vercel-storage.com/IMG_1088-RcfcDKjg7yVN4GzPkFxzfgK6I5Ckwk.jpeg"
     }
 })
 .then(function(response) {
     console.log(response.data);
 })
 .catch(function(error) {
-    console.log(error.message);
+    console.log(error);
 });
