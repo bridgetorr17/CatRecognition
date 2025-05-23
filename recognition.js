4ab918e697ff10364092a240beca1fd45f22fb95
bridgetorr17 <bridgetorr1902@gmail.com>
2025-05-23 16:01:51 -0400

    recognition logic

--- recognition.js
+++ recognition.js
@@ -11,31 +11,27 @@ axios({
     url: "https://serverless.roboflow.com/cats-1dq9b/4",
     params: {
         api_key: process.env.CAT_RECOGNITON_KEY,
-        image: "https://l4e1pa0te997vcur.public.blob.vercel-storage.com/IMG_2707-ZRaO2hnMt6dFqt00Zcr1toRsPFHmib.webp"
+        image: "https://l4e1pa0te997vcur.public.blob.vercel-storage.com/stock3-pemvqFXTgJscWCAAhXyU3jquNSj8sf.jpg"
     }
 })
 .then(function(response) {
-    console.log(response.data.predictions);
+    console.log(response.data);
     if(response.data.predictions.length === 0) isCat = true;
     else {
-        response.data.predictions.forEach(item => {
-            //if the model might have detected a cat, 
-            //and it is even a little sure
-            if((item.class === 'cat' || item.class === 'pepe') && item.confidence > 0.50){  
+        for(const item of response.data.predictions){
+            if((item.class === 'cat' || item.class === 'pepe') && item.confidence > 0.5){
                 isCat = true;
-                console.log('lets say this is a cat');
+                console.log('this is a cat');
+                break;
             }
-            
-            //if the model detected something that it is 
-            //very confident is NOT a cat, and there is 
-            //nothing that even looks like a cat
-            else if (item.class === 'not_cat' && item.confidence > 0.8){
-                isNotCat = true;
-                console.log('whatever picked up here is def not a cat')
-            }
-        });
+        }
     }
-    
+    if(isCat){
+        console.log(`send ${response.data.predictions}to database`)
+    }
+    else{
+        console.log('not sending this')
+    }
 })
 .catch(function(error) {
     console.log(error);
